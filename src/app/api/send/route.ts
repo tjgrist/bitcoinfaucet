226bdc076f159581c10d3402ff5d049f92d81f06
tj.gristalaskaair.com <tj.grist@alaskaair.com>
2024-04-11 18:27:39 -0700

    Refactor addIp function to include faucet limit in route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDoc, doc } from 'firebase/firestore';
 import db from '@/lib/firebase';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -8,14 +8,25 @@ const fromEmail = process.env.FROM_EMAIL as string;
 const myEmail = process.env.MY_EMAIL as string;
 
 async function addIp(ip: string) {
-  try {
-    const docRef = await addDoc(collection(db, "ips"), {
-      address: ip
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (error) {
-    console.error("Error adding document: ", error);
-  }
+    try {
+        const faucetRef = doc(db, "config", "faucet"); 
+        const docSnap = await getDoc(faucetRef);
+
+        if (!docSnap.exists()) {
+            console.log("No faucet limit doc");
+            return;
+        }
+
+        const docRef = await addDoc(collection(db, "ips"), {
+            ip: ip,
+            timestamp: new Date(),
+            tbtc: docSnap.data().limit
+        });
+
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
@@ -26,7 +37,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     if (!ip) return NextResponse.error();
 
     // add the ip to the firestore database collection 'ips'
-    await addIp(ip)
+    await addIp(ip, )
 
     try {
         const responseToMe = await resend.emails.send({
