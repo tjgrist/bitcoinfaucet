01e165e2cc73581ccf6cba7df8a0516e00bfea7a
tj.gristalaskaair.com <tj.grist@alaskaair.com>
2024-04-12 17:57:50 -0700

    Refactor error handling in route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -10,13 +10,13 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     const { address } = await req.json();
 
-    if (!address) return NextResponse.json({ error: 'No address found.' });
+    if (!address) return NextResponse.error();
 
-    if (!validate(address, 'testnet' as Network)) return NextResponse.json({ error: 'Invalid address' });
+    if (!validate(address, 'testnet' as Network)) return NextResponse.error();
 
     const ip = req.headers.get('x-real-ip') || req.headers.get('x-forwarded-for') || req.ip;
 
-    if (!ip) return NextResponse.json({ error: 'No IP found.' });
+    if (!ip) return NextResponse.error();
 
     const limit = await faucetLimit();
 
@@ -24,7 +24,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
         await getLatestTransactionByIp(ip);
     }
     catch (error) {
-        return NextResponse.json({ error: 'Too many requests. Please try again later.'});
+        return NextResponse.error();
     }
     
     try {
@@ -33,7 +33,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     }
     catch (error) {
         console.error('Error creating withdrawal', error);
-        return NextResponse.json({ error: 'Error creating withdrawal.'});
+        return NextResponse.error();
     }
 
     try {
@@ -41,6 +41,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
         return NextResponse.json(responseToMe);
     } catch (error) {
-        return NextResponse.json({ error });
+        return NextResponse.error();
     }
 }
