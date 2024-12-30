a52734152499088b32afae30c7220c73ab3d098d
tj.gristalaskaair.com <tj.grist@alaskaair.com>
2024-12-30 12:40:12 -0600

    update limit

--- src/lib/transactionLimit.ts
+++ src/lib/transactionLimit.ts
@@ -1,7 +1,15 @@
-import { getBalance } from "./balance";
+// import { getBalance } from "./balance";
+
+// // 1% of balance for the limit
+// export async function getTransactionLimit() {
+//     const balance = await getBalance();
+//     return balance * 0.001;
+// }
+
+import faucetConfig from "./faucetLimit";
 
 // 1% of balance for the limit
 export async function getTransactionLimit() {
-    const balance = await getBalance();
-    return balance * 0.001;
+    const config = await faucetConfig();
+    return config.limit;
 }
