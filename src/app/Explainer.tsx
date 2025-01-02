2103a93c96f9333972a628163ccde349b8fd532b
tj.gristalaskaair.com <tj.grist@alaskaair.com>
2025-01-02 12:24:46 -0600

    Update tBTC display to reflect correct limit in satoshis

--- src/app/Explainer.tsx
+++ src/app/Explainer.tsx
@@ -9,7 +9,7 @@ export default function Explainer() {
         <div className="container mx-auto rounded-lg mb-20">
             <h1 className="font-bold mb-2 text-7xl">Get Free tBTC!</h1>
             <p className="text-sm mb-2">Develop and test Bitcoin apps safely with testnet Bitcoin (tBTC).</p>
-            <p className="text-sm mb-2">Send {data?.limit} tBTC to yourself today!</p>
+            <p className="text-sm mb-2">Send {data?.limit * 1e-8} tBTC to yourself today!</p>
         </div>
     )
 }
